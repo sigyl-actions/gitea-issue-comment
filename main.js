05b13f43e2e4e73fa96281a6a65e5a45f493d0ca
gilesb <giles.bradshaw@sigyl.com>
2023-11-02 09:13:51 +0000

    initial commit

---
+++ main.js
@@ -0,0 +1,45 @@
+import fs from 'fs';
+import { Blob } from 'buffer';
+
+
+import core from '@actions/core';
+import github from '@actions/github';
+
+import { GiteaApi } from 'gitea-api';
+
+async function run() {
+  try {
+    const serverUrl = core.getInput('serverUrl')
+      || (github.context.runId && github.context.serverUrl)
+      || process.argv[3]
+
+    const client = new GiteaApi({
+      BASE: `${serverUrl}/api/v1`,
+      WITH_CREDENTIALS: true,
+      TOKEN: core.getInput('token') || process.argv[2],
+    });
+    const [owner, repo] = (
+      core.getInput('repository')
+      || github?.context?.payload?.repository?.full_name
+      || process.argv[4]
+    ).split("/");
+
+    await client
+      .issue
+      .issueCreateComment({
+        owner,
+        repo,
+        index: core.getInput('id') || process.argv[5],
+        body: {
+          description: core.getInput('description') || process.argv[6],
+          body: core.getInput('body') || process.argv[7],
+        },
+      })
+  }
+  catch (error) {
+    console.error(error)
+    core.setFailed(error.message);
+  }
+}
+
+run()
